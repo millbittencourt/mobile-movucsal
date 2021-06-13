3e62c85da9cb05a63d450bc0299838b578218b40
Jamille Bittencourt <mille.bittencourt@gmail.com>
2021-06-12 21:21:44 -0300

    cleans path responde

--- lib/models/pathResponse.dart
+++ lib/models/pathResponse.dart
@@ -1,23 +1,14 @@
 class PathResponse {
 
-  final int profundidade;
-  final double custo;
-  final double heuristica;
   final List pontosVisitados;
 
   PathResponse({
-    required this.profundidade,
-    required this.custo,
-    required this.heuristica,
     required this.pontosVisitados,
   });
 
   factory PathResponse.fromJson(Map<String, dynamic> json) {
     return PathResponse(
-      profundidade: json['profundidade'],
-      custo: json['custo'],
-      heuristica: json['heuristica'],
-      pontosVisitados: json['pontosVisitados1'],
+      pontosVisitados: json['pontosVisitados'],
     );
   }
 
